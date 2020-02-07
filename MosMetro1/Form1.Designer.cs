6933c90129471e20e7c75050f97c63a866120201
molodec1337322 <sshibanov1@yandex.ru>
2020-02-07 12:48:10 +0300

    8

--- MosMetro1/Form1.Designer.cs
+++ MosMetro1/Form1.Designer.cs
@@ -199,7 +199,7 @@
             this.ControlGroup.Controls.Add(this.DestinationGroup);
             this.ControlGroup.Controls.Add(this.DepartureGroup);
             this.ControlGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.ControlGroup.Location = new System.Drawing.Point(702, 11);
+            this.ControlGroup.Location = new System.Drawing.Point(12, 12);
             this.ControlGroup.Name = "ControlGroup";
             this.ControlGroup.Size = new System.Drawing.Size(270, 758);
             this.ControlGroup.TabIndex = 13;
@@ -210,7 +210,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 781);
+            this.ClientSize = new System.Drawing.Size(295, 781);
             this.Controls.Add(this.ControlGroup);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
